035158d4d2af29fe7dd6ec94fc2416f045dc327b
Tobias <tobias.monyk@hotmail.com>
2026-03-29 15:55:45 +0200

    - implemented simple navbar component

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthStore } from '../../states/auth-store';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class NavbarComponent {
+  authStore = inject(AuthStore);
 
+  logout(): void {
+    this.authStore.logout();
+  }
 }
