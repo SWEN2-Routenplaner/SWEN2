f3674337a843eb4ae321c08d0094704680918df2
Tobias <tobias.monyk@hotmail.com>
2026-06-24 20:58:10 +0200

    - added SearchController.java

---
+++ backend/src/main/java/at/fhtw/tourplanner/business/service/SearchService.java
@@ -0,0 +1,11 @@
+package at.fhtw.tourplanner.business.service;
+
+import at.fhtw.tourplanner.presentation.dto.response.SearchResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+    public SearchResponse search(String subject, String q) {
+        return null;
+    }
+}
---
+++ backend/src/main/java/at/fhtw/tourplanner/presentation/controller/SearchController.java
@@ -0,0 +1,27 @@
+package at.fhtw.tourplanner.presentation.controller;
+
+import at.fhtw.tourplanner.business.service.SearchService;
+import at.fhtw.tourplanner.presentation.dto.response.SearchResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/search")
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final SearchService searchService;
+
+    @GetMapping
+    public ResponseEntity<SearchResponse> search(
+            @RequestParam String q,
+            @AuthenticationPrincipal Jwt jwt) {
+        return ResponseEntity.ok(searchService.search(jwt.getSubject(), q));
+    }
+}
