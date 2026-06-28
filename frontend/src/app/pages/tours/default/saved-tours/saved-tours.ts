d80f8e3552ab846ac785b00cbf53d944cf77888f
Elias-Haider <haider.elias2003@gmail.com>
2026-06-28 21:16:06 +0200

    Version 1 of Familiy Friendly calculator and popularity

--- frontend/src/app/pages/tours/default/saved-tours/saved-tours.ts
+++ frontend/src/app/pages/tours/default/saved-tours/saved-tours.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {Tour} from '../../../../models/tour.model';
 import {toObservable} from '@angular/core/rxjs-interop';
 import {Router} from '@angular/router';
+import {TourLogsStore} from '../../../../states/tour-logs.store';
 
 @Component({
   selector: 'app-saved-tours',
@@ -16,6 +17,7 @@ import {Router} from '@angular/router';
 export class SavedToursComponent {
   toursStore = inject(ToursStore);
   router = inject(Router);
+  tourLogsStore = inject(TourLogsStore);
 
   activeTourId = signal<number | null>(null)
   tours = this.toursStore.allTours;
@@ -36,5 +38,22 @@ export class SavedToursComponent {
   }
   showLogs(id:number){
     this.router.navigate(['', 'tourlogs', id]);
+  }
+  //Calculates popularity of a tour based on the number of logs associated with it
+  calculatePopularity(tourId: number): string {
+    const logs = this.tourLogsStore.getLogsByTourId(tourId);
+    if(logs.length === 0) {
+      return "No logs yet";
+    }
+    if(logs.length < 3) {
+      return "Not very popular";
+    }
+    if(logs.length < 5) {
+      return "Somewhat popular";
+    }
+    if(logs.length < 10) {
+      return "Popular";
+    }
+    return "Very popular";
   }
 }
