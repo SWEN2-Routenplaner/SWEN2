e91939a56191a9865dbc7d629904b8bcc536595d
Tobias <tobias.monyk@hotmail.com>
2026-06-30 11:52:02 +0200

    - added empty service class for tour; added tour.dto and adjusted tour.model

--- frontend/src/app/models/tour.model.ts
+++ frontend/src/app/models/tour.model.ts
@@ -1,12 +1,13 @@
-export type TransportMode = 'CAR' | 'BIKE' | 'WALK' | null;
+export type TransportType = 'CAR' | 'BIKE' | 'WALK' | null;
 
 export type Tour = {
   id: number;
   from: string;
-  transportMode: TransportMode;
+  TransportType: TransportType;
   to: string;
   name: string;
   description: string;
   distance: number;
   estimatedDuration: number;
+  routeInformation: string;
 }
